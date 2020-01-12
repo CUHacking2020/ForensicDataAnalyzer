acb342b43cab3cc9e5cb11d8992af19ea8c6d281
CUHacking2020 <59798640+CUHacking2020@users.noreply.github.com>
2020-01-12 11:14:04 -0500

    Update forensicDataAnalyzer.py

--- Murder on the 2nd Floor/forensicDataAnalyzer.py
+++ Murder on the 2nd Floor/forensicDataAnalyzer.py
@@ -148,84 +148,84 @@ class GUI:
 
         photo2 = PhotoImage(file = "jason.png")
         img2 = photo2.subsample(3,3)
-        sus2 = Button(window,text="  Jason\n  Hotel Guest", height = 200, width = 300, image = img2, compound = "left",
+        sus2 = Button(window,text="  Jason\n  Hotel Guest\n  (Suspect #1)", height = 200, width = 300, image = img2, compound = "left",
                       command=lambda:self.suspect_profile_window("Jason"))
         sus2.image = img2
         sus2.grid(row=0, column=1)
 
         photo3 = PhotoImage(file = "thomas.png")
         img3 = photo3.subsample(3,3)
-        sus3 = Button(window,text="  Thomas\n  Hotel Guest", height = 200, width = 300, image = img3, compound = "left",
+        sus3 = Button(window,text="  Thomas\n  Hotel Guest\n   (Suspect #3)", height = 200, width = 300, image = img3, compound = "left",
                       command=lambda:self.suspect_profile_window("Thomas"))
         sus3.image = img3
         sus3.grid(row=0,column=3)
 
         photo4 = PhotoImage(file = "rob.png")
         img4 = photo4.subsample(3,3)
-        sus4 = Button(window,text="  Rob\n  Hotel Guest", height = 200, width = 300, image = img4, compound = "left",
+        sus4 = Button(window,text="  Rob\n  Hotel Guest\n   (Suspect #5)", height = 200, width = 300, image = img4, compound = "left",
                       command=lambda:self.suspect_profile_window("Rob"))
         sus4.image = img4
         sus4.grid(row=1,column=1)
 
         photo5 = PhotoImage(file = "kristina.png")
         img5 = photo5.subsample(3,3)
-        sus5 = Button(window,text="  Kristina\n  Hotel Guest", height = 200, width = 300, image = img5, compound = "left",
+        sus5 = Button(window,text="  Kristina\n  Hotel Guest\n   (Suspect #4)", height = 200, width = 300, image = img5, compound = "left",
                       command=lambda:self.suspect_profile_window("Kristina"))
         sus5.image = img5
         sus5.grid(row=1,column=0)
 
         photo6 = PhotoImage(file = "marc_andre.png")
         img6 = photo6.subsample(3,3)
-        sus6 = Button(window,text="  Marc-Andre\n  Hotel Staff", height = 200, width = 300, image = img6, compound = "left",
+        sus6 = Button(window,text="  Marc-Andre\n  Hotel Staff\n   (Suspect #2)", height = 200, width = 300, image = img6, compound = "left",
                       command=lambda:self.suspect_profile_window("Marc-Andre"))
         sus6.image = img6
         sus6.grid(row=0,column=2)
 
         photo7 = PhotoImage(file = "dave.png")
         img7 = photo7.subsample(3,3)
-        sus7 = Button(window,text="  Dave\n  Hotel Staff", height = 200, width = 300, image = img7, compound = "left",
+        sus7 = Button(window,text="  Dave\n  Hotel Staff\n   (Suspect #6)", height = 200, width = 300, image = img7, compound = "left",
                       command=lambda:self.suspect_profile_window("Dave"))
         sus7.image = img7
         sus7.grid(row=1,column=2)
 
         photo8 = PhotoImage(file = "selena.png")
         img8 = photo8.subsample(3,3)
-        sus8 = Button(window,text="  Salina\n  Hotel Staff", height = 200, width = 300, image = img8, compound = "left",
+        sus8 = Button(window,text="  Salina\n  Hotel Staff\n   (Suspect #10)", height = 200, width = 300, image = img8, compound = "left",
                       command=lambda:self.suspect_profile_window("Salina"))
         sus8.image = img8
         sus8.grid(row=2,column=2)
 
         photo9 = PhotoImage(file = "harrison.png")
         img9 = photo9.subsample(3,3)
-        sus9 = Button(window,text="  Harrison\n  Hotel Staff", height = 200, width = 300, image = img9, compound = "left",
+        sus9 = Button(window,text="  Harrison\n  Hotel Staff\n   (Suspect #11)", height = 200, width = 300, image = img9, compound = "left",
                       command=lambda:self.suspect_profile_window("Harrison"))
         sus9.image = img9
         sus9.grid(row=2,column=3)
 
         photo10 = PhotoImage(file = "eugene.png")
         img10 = photo10.subsample(3,3)
-        sus10 = Button(window,text="  Eugene\n  Non-Guest", height = 200, width = 300, image = img10, compound = "left",
+        sus10 = Button(window,text="  Eugene\n  Non-Guest\n   (Suspect #9)", height = 200, width = 300, image = img10, compound = "left",
                        command=lambda:self.suspect_profile_window("Eugene"))
         sus10.image = img10
         sus10.grid(row=2,column=1)
 
         photo11 = PhotoImage(file = "alok.png")
         img11 = photo11.subsample(3,3)
-        sus11 = Button(window,text="  Alok\n  Non-Guest", height = 200, width = 300, image = img11, compound = "left",
+        sus11 = Button(window,text="  Alok\n  Non-Guest\n   (Suspect #7)", height = 200, width = 300, image = img11, compound = "left",
                        command=lambda:self.suspect_profile_window("Alok"))
         sus11.image = img11
         sus11.grid(row=1,column=3)
 
         photo12 = PhotoImage(file = "james.png")
         img12 = photo12.subsample(3,3)
-        sus12 = Button(window,text="  James\n  Non-Guest", height = 200, width = 300, image = img12, compound = "left",
+        sus12 = Button(window,text="  James\n  Non-Guest\n   (Suspect #8)", height = 200, width = 300, image = img12, compound = "left",
                        command=lambda:self.suspect_profile_window("James"))
         sus12.image = img12
         sus12.grid(row=2,column=0)
 
         Label(window, text="").grid(row=3,column=3)
 
-        Label(window, text= "The suspect list is in order from most to least suspicious based off their data.\n Click profile to analize their individual data.\n To view phone calls, click the All Data tab.",
+        Label(window, text= "The suspect list is in order from most to least suspicious based off their data.\n Click profile to analize or search through their individual data.\n To view phone calls, click the All Data tab.",
               font=("Courier", 12)).grid(row = 3, columnspan = 3)
 
         Button(window, text="   All Data   ",command=lambda:self.data_window()).grid(row=3,column=3)
@@ -246,3 +246,4 @@ root = Tk()
 root.geometry("1280x700")
 forensic_data_analyzer = GUI(root)
 root.mainloop()
+
